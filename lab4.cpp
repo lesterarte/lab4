f7357ade34546c4e8072b9b99a9338621e1a2cb3
lesterarte <lesterarte007@unitec.edu>
2016-02-12 16:21:33 -0600

    Funciones de Tipo de juego

--- lab4.cpp
+++ lab4.cpp
@@ -1,3 +1,4 @@
+#include<cstdlib>
 #include<iostream>
 using std::endl; 
 using std::cout; 
@@ -7,13 +8,9 @@ void crearcubo();
 int main(int argc, char*argv[]){
 	int sub=15;
 	crearcubo();
+	srand(time(NULL)); 
  	return 0;		
 }
-
-
-<<<<<<< HEAD
-=======
-
 
 
 void crearcubo(){
@@ -30,4 +27,124 @@ void crearcubo(){
 	}
 
 }
->>>>>>> 7030fe23af5305fdd0391278eda3b19c98fa960b
+
+void iniciarcubo(int*** cubo,int size){
+
+	for(int i=0; i<size; i++){
+		for(int j=0; i<size; j++){
+			for(int k=0; i<size; k++){
+				cubo[i][j][k]= 0;
+
+			}
+		}	
+	}
+	
+}
+
+
+
+ void llenar(int*** cubo,int size, int submarinos){
+
+	 while(submarinos !=0){
+
+		int i=rand()%size; 
+		int j=rand()%size; 
+		int k= rand()%size;  
+		 
+		if(cubo[i][j][k]== 0){
+
+			cubo[i][j][k]= 1; 		
+			submarinos--; 
+		}
+			     
+	}		 
+}
+
+
+void ataquenormal(int*** cubo, int x, int y, int z){
+	
+	if(cubo[x][y][z]== 0){
+		cout<<"HAHA no destruiste nada"<<endl; 
+	}
+	else{
+		cout<<"felicidades destruiste un Submarino Oponente"<<endl;
+		cubo[x][y][z]=0; 
+		 
+
+	}
+			
+}
+
+void ataquewive(int*** cubo , int z,int size, int const){
+
+	int cont=0; 
+	
+	if(const==1){	
+	for(int i=0; i<size; i++){
+		for(int j=0; j<size; j++){
+			if(cubo[i][j][z] !=0){
+				 cubo[i][j][z]=0;
+				 cont++;	
+			}	
+
+			
+		}
+	}
+
+	if(cont==0){
+		cout<<"No destruiste nada mijo"<<endl; 
+	}
+	else{
+		cout<<"Destruiste "<<cont<<"del oponente FELICIDADES!!!!"<<endl; 
+	}
+
+	}//fin if
+
+	if(const==2){	
+	for(int i=0; i<size; i++){
+		for(int j=0; j<size; j++){
+			if(cubo[i][z][j] !=0){
+				 cubo[i][z][j]=0;
+				 cont++;	
+			}	
+
+			
+		}
+	}
+
+	if(cont==0){
+		cout<<"No destruiste nada mijo"<<endl; 
+	}
+	else{
+		cout<<"Destruiste "<<cont<<"del oponente FELICIDADES!!!!"<<endl; 
+	}
+
+	}//fin if
+
+	if(const==3){	
+	for(int i=0; i<size; i++){
+		for(int j=0; j<size; j++){
+			if(cubo[z][i][j] !=0){
+				 cubo[z][i][j]=0;
+				 cont++;	
+			}	
+
+			
+		}
+	}
+
+	if(cont==0){
+		cout<<"No destruiste nada mijo"<<endl; 
+	}
+	else{
+		cout<<"Destruiste "<<cont<<"del oponente FELICIDADES!!!!"<<endl; 
+	}
+
+	}//fin if
+	
+		
+}//fin void 
+
+
+
+
